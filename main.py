4a2a2be9a941fb1c790308c5baa0a00cea010d7c
ya-whiteflower <ya-whiteflower@yandex.ru>
2024-05-26 20:13:59 +0300

    flowers

--- main.py
+++ main.py
@@ -4,13 +4,12 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def hi(name):
     # Use a breakpoint in the code line below to debug your script.
-    print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('derevo')
+print(hi("develop"))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
